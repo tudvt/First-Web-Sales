22ff7f31a06574853a97ad920c7e9d721aab04db
Tuong tu <dotuongtu196@gmail.com>
2018-08-11 11:38:32 +0700

    Statement Vs PreparedStatement

--- src/StamentVsPrepareStament/StamentVsPrepareStament.java
+++ src/StamentVsPrepareStament/StamentVsPrepareStament.java
@@ -1,4 +1,13 @@
-
+	// cả 2 phương thức del và remove đều sử dụng để xóa 1 Object tuy nhiên dùng 2
+	// kiểu dữ liệu khác nhau là Statement và PreparedStatement
+	// nó xẩy ra do việc truyền giá trị trực tiếp vào câu lệnh SQL.
+    //việc truyền trực tiếp giá trị vào khi sử dụng đối tượng Statement. Còn với PreparedStatement thì đã khắc phục được điều đó.
+	// Với PreparedStatement mỗi giá trị truyền vào đều phải thông qua việc check,
+	// kiểm tra kiều với các hàm setString(), setInt() ..
+	
+	//Kết luận:
+	//Với những trường hợp SQL không cần truyền vào tham số thì sẽ sử dụng đối tượng Statement
+	//Với nhưng trường hợp SQL cần truyền vào tham số để filter trong việc select, delete, hay update thì sẽ sử dụng đối tượng PreparedStatement.
 
 //Statement
 public void excuteSql(String sql) throws Exception {
